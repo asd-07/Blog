ae8cb809ce0193ceae1fa3ed60292f53ed7e1a06
Anup Singh <anup32633@gmail.com>
2024-02-19 18:56:47 +0530

    final commit

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,14 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import Button from "./Button"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Select from "./Select"
 
 export {
     Header,
@@ -11,5 +19,13 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
-}
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    Button,
+    PostCard,
+    AuthLayout,
+    Input,
+    Select
+} 
